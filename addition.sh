b5eeaedfb900b502fb67247ff85cc2749f6bcb2f
shubham932000 <ssv932000@gmail.com>
2024-01-24 09:44:34 +0530

    first commit

---
+++ addition.sh
@@ -0,0 +1,6 @@
+read -p "ENTER THE FIRST NUM: "  x
+read -p "ENTER THE SECOND NUM: " y
+echo $x
+echo $y
+z=$(( $x + $y ))
+echo sum of number is : $z
---
+++ ifstatement.sh
@@ -0,0 +1,6 @@
+read -p "ENTER THE NUM: " a
+read -p "ENTER THE NUM: " b
+if [ $a -gt $b ]
+then
+echo $a is greater than $b
+fi
